9849419790600326d7a900ddf3beb9fda08bf411
killerkodycodes <killerbyrd12@gmail.com>
2024-05-20 20:33:28 -0500

    initial skeleton and setup.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: HomePageComponent},
+  {path: "about", component: AboutPageComponent},
+  {path: "contact", component: ContactPageComponent}
+];
