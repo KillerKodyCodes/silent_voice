ca49d4e410b721d6ac558807d48722706a25fff9
killerkodycodes <killerbyrd12@gmail.com>
2024-05-23 20:54:21 -0500

    feat: added a mobile-friendly navigation bar.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
@@ -9,4 +9,21 @@ import { RouterLink } from '@angular/router';
 })
 export class NavComponent {
 
+  isMenuOpen = false;
+
+  constructor(private router: Router){};
+
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(event: Event){
+    this.isMenuOpen = false;
+    event.stopPropagation(); // Prevent the event from bubbling up to the li
+  }
+
+  navigate(route:string){
+    this.router.navigate([route]);
+    this.isMenuOpen = false;
+  }
 }
